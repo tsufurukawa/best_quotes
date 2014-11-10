32cc1b4174ea5ca1c6a123bffaf468c9030c06ba
Tsubasa Furukawa <tfurukawa824@gmail.com>
2014-11-09 16:42:52 -0800

    Created a QuotesController and a single 'a_quote' action where '/quotes/a_quote' is routed to

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "https://rubygems.org"
 
 gem 'rulers'
+gem 'pry'
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,7 @@
 # Note we have to explicitly 'require' the gem manually - in Rails all gems are autoloaded
-require 'rulers' 
+require "rulers"
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+require "quotes_controller"
 
 module BestQuotes
   class Application < Rulers::Application
